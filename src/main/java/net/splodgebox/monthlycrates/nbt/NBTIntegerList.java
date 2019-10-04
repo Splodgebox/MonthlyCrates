54a4cd38acc5ba18d78fab7042740571c522c24b
Splodgebox <splodegbox786@gmail.com>
2019-10-04 14:35:30 +0100

    Added the new NBT system and added some fixes

---
+++ src/main/java/net/splodgebox/monthlycrates/nbt/NBTIntegerList.java
@@ -0,0 +1,42 @@
+package de.tr7zw.changeme.nbtapi;
+
+import java.lang.reflect.InvocationTargetException;
+
+import de.tr7zw.changeme.nbtapi.utils.nmsmappings.ClassWrapper;
+import de.tr7zw.changeme.nbtapi.utils.nmsmappings.ReflectionMethod;
+
+/**
+ * Integer implementation for NBTLists
+ * 
+ * @author tr7zw
+ *
+ */
+public class NBTIntegerList extends NBTList<Integer> {
+
+	protected NBTIntegerList(NBTCompound owner, String name, NBTType type, Object list) {
+		super(owner, name, type, list);
+	}
+
+	@Override
+	protected Object asTag(Integer object) {
+		try {
+			return ClassWrapper.NMS_NBTTAGINT.getClazz().getConstructor(int.class).newInstance(object);
+		} catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException
+				| NoSuchMethodException | SecurityException e) {
+			throw new NbtApiException("Error while wrapping the Oject " + object + " to it's NMS object!", e);
+		}
+	}
+
+	@Override
+	public Integer get(int index) {
+		try {
+			Object obj = ReflectionMethod.LIST_GET.run(listObject, index);
+			return Integer.valueOf(obj.toString());
+		} catch (NumberFormatException nf) {
+			return 0;
+		} catch (Exception ex) {
+			throw new NbtApiException(ex);
+		}
+	}
+
+}
